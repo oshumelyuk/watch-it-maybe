fdb03449ec7f6bab999ee4fc2e8b715a5c5fae11
Olha Shumeliuk <oshumeliuk@lohika.com>
2019-10-09 23:01:36 +0300

    add movie tile

---
+++ src/components/Tile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tile">
+    This is tile with title {{title}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Tile extends Vue {
+  @Prop() public title!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Movies.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="movies">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Tile title="First movie"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Tile from '@/components/Tile.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Tile,
+  },
+})
+export default class Movies extends Vue {}
+</script>
